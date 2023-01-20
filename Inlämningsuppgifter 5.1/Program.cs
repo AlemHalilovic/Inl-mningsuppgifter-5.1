de5b4d3f315b9682d141aa61df68440b2ac241c9
Admin <Admin@TEB127022>
2023-01-20 10:36:12 +0100

    Add project files.

---
+++ Inlämningsuppgifter 5.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Inlämningsuppgifter5_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            string[] M = new string[3];
+            for (int i = 0; i < 3; i++)
+            {
+
+                Console.WriteLine("Skriv in en mening");
+                M[i] = Console.ReadLine();
+
+            }
+            Console.WriteLine("Dessa meningarna skrev du");
+            for (int i = 0; i < 3; i++) 
+                Console.WriteLine(M[i]);
+           
+
+
+
+
+
+
+        }
+    }
+}
